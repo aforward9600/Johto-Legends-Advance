cc9e6df98a712272d97515e2bc3bb0b1d201f231
Aaron <aaron@LAPTOP-I9IMHBDO.localdomain>
2023-05-18 19:54:18 -0400

    Merge branch 'dynamic-ow-pals' of https://github.com/ExpoSeed/pokeemerald into upcoming

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), MAP_NUM(LITTLEROOT_TOWN_BRENDANS_HOUSE_2F), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
